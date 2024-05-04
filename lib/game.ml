4d393d3610f03c0705a8e35ff148f5c0ff664de5
Mauritz <mauritz.sjodin3@gmail.com>
2024-05-04 11:21:59 +0200

    feat: function for calculating wpm

--- lib/game.ml
+++ lib/game.ml
@@ -1,1 +1,8 @@
-let str = "test"
+let str = "this is a test"
+
+let words s = List.length (String.split_on_char ' ' s)
+
+let wpm str sec = 
+    let n_words = words str in
+    let min = sec /. 60.0 in
+    float_of_int (n_words) /. min
