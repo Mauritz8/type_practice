6b471362f796cb883e396c69df21f278f09cf323
Mauritz <mauritz.sjodin3@gmail.com>
2024-05-04 10:02:11 +0200

    add simple game loop

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,17 @@
-let () = print_endline "Hello, World!"
+let game_loop str =
+    let rec aux process =
+        if (String.equal str process) then true
+        else
+            let input = read_line () in
+            let ch = String.get input 0 in
+            let compare_ch = String.get str (String.length process) in
+            if Char.equal ch compare_ch then
+                aux (process ^ String.make 1 ch)
+            else
+                aux process 
+    in aux ""
+
+let str = "test"
+let () = print_endline str
+let success = game_loop str
+let () = print_endline (if success then "win" else "lose")
